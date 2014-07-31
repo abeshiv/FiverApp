801a8d1f26aea5d67bcae3d71161963e7c18e967
abeshiv <van.abeshi@gmail.com>
2014-07-31 13:26:37 -0400

    Some small insignificant changes

--- FiverApp/FiverApp/Controllers/HomeController.cs
+++ FiverApp/FiverApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace FiverApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "More about us.";
 
             return View();
         }
