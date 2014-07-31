1f1c6c4b7c3a92af1c668725000ac3316ccb8828
Jon Camunag <jcamunag@gmail.com>
2014-07-31 14:12:27 -0400

    Fixed error

--- FiverApp/FiverApp/Model1.Designer.cs
+++ FiverApp/FiverApp/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\J\Documents\Visual Studio 2013\Projects\FiverApp\FiverApp\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\J\Documents\GitHub\FiverApp\FiverApp\FiverApp\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
